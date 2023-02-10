2e22fbf8dd95dd9553f50075cafcc69704bb47b5
Muguntha96 <123465880+Muguntha96@users.noreply.github.com>
2023-02-10 07:01:21 +0530

    Add files via upload

---
+++ BankInfo.java
@@ -0,0 +1,19 @@
+package org.bank;
+
+public class BankInfo extends AxisBank {
+	public void saving() {
+		System.out.println("Savings:8%");
+
+	}
+	public void fixed() {
+		System.out.println("fixed:6%");
+
+	}
+	public static void main(String[] args) {
+		BankInfo b=new BankInfo();
+		b.fixed();
+		b.saving();
+		b.deposit();
+	}
+
+}
---
+++ Drop.java
@@ -0,0 +1,17 @@
+package org.bank;
+
+public class Drop {
+
+	public static void main(String[] args) {
+		
+	
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\\\Users\\\\ramku\\\\eclipse-workspace\\\\FaceBook\\\\driver\\\\chromedriver.exe");
+		
+		
+		
+		
+		
+	}
+
+}
